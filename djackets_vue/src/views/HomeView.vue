a457b02c17331b4a2bac628c0258d27a6f54bacc
michelclemer <michelclemermonteirodeolino@gmail.com>
2022-04-10 10:16:44 -0300

    :sparkles: feat: Adicionando listagem de produtos no frontend

--- djackets_vue/src/views/HomeView.vue
+++ djackets_vue/src/views/HomeView.vue
@@ -1,15 +1,67 @@
 <template>
   <div class="home">
-    Home
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Welcomer to Djacket
+          </p>
+        <p class="subtitle">
+          The best jacket store online
+        </p>
+      </div>
+    </section>
+
+  <div class="columns is-multiline">
+    <div class="column is-12">
+      <h2 class="is-size-2 has-text-centered">Latest products</h2>
+    </div>
+
+    <div
+     class="column is-3"
+     v-for="product in latestProducts"
+     v-bind:key="product.id"
+    >
+    <div class="box">
+      <figure class="image mb-4">
+        <img :src="product.get_thumbnail" >
+      </figure>
+      <h3 class="is-size-4">{{ product.name }}</h3>
+      <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+
+      View details
+    </div>
+    </div>
+  </div>
+
   </div>
 </template>
 
 <script>
-
+import axios from 'axios'
 export default {
   name: 'Home',
+  data() {
+    return{
+      latestProduct: []
+    }
+  },
   components: {
    
+  },
+  mounted(){
+    this.getLatestProduct() 
+  },
+  methods: {
+    getLatestProduct(){
+      axios
+      .get('/api/v1/latest-products/')
+      .then(response => {
+        this.latestProduct = response.data
+      })
+      .catch(error => {
+        console.log("Error aqui => ", error)
+      })
+    }
   }
 }
 </script>
